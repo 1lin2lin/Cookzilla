0f8b1e319624c8dfc73c5db4cb176539f3cbc506
hualinliu <hualin92@gmail.com>
2022-12-16 17:40:16 -0500

    updated search by key word to search through title instead of steps

--- init.py
+++ init.py
@@ -6,31 +6,6 @@ import pymysql.cursors
 #for uploading photo:
 from app import app
 from werkzeug.utils import secure_filename
-#
-# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-#
-# def allowed_image(filename):
-#
-#     if not "." in filename:
-#         return False
-#
-#     ext = filename.rsplit(".", 1)[1]
-#
-#     if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
-#         return True
-#     else:
-#         return False
-#
-#
-# def allowed_image_filesize(filesize):
-#
-#     if int(filesize) <= app.config["MAX_IMAGE_FILESIZE"]:
-#         return True
-#     else:
-#         return False
-#
-# def allowed_file(filename):
-#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #Configure MySQL
 conn = pymysql.connect(host='localhost',
